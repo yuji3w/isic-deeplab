d1b003a0ca6926abc98c8ac14edd6c874e9f3407
ISIC <yuji3wang@gmail.com>
2018-07-28 00:07:06 -0400

    modified utils/rename for compatibility

--- utils/rename.py
+++ utils/rename.py
@@ -1,11 +1,11 @@
 import os
-path = r'C:\Users\isica\Desktop\gt'
+#path = r'C:\Users\isica\Desktop\gt'
+path = r"C:\Users\isica\Desktop\isic_resizing\labels"
 
 count = 0
 for filename in os.listdir(path):
-    num, suffix = filename[:-6].split('_')
-    num = str(count).zfill(6)
-    new_filename = num + "_prediction.png"
+    prefix, num, suffix = filename.split('_')
+#    num = str(count).zfill(6)
+    new_filename = prefix +  "_" + num + ".png"# + "_prediction.png"
     os.rename(os.path.join(path, filename), os.path.join(path, new_filename))
-    count += 1
-   
+#    count += 1
