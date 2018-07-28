5842efcbeb30e1e8a7ac5e6620ca64ae6d42f7ee
ISIC <yuji3wang@gmail.com>
2018-07-28 00:08:01 -0400

    added functionality to resize to max default dims

--- utils/resizeToMax.py
+++ utils/resizeToMax.py
@@ -1,8 +1,8 @@
 import os, glob
 import cv2
-ulpath = r"C:\Users\Yujie Wang\Desktop\testfolder"
+ulpath = r"C:\Users\isica\Desktop\isic_resizing\labels"
 import numpy as np
-for infile in glob.glob( os.path.join(ulpath, "*.png") ):
+for infile in glob.glob( os.path.join(ulpath, "*.png")):
 	im = cv2.imread(infile)
 	height, width, channels = im.shape
 	small = cv2.resize(im, (0,0), fx=513/width, fy=513/height)
