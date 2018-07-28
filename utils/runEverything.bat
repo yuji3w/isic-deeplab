de60ab9ca12a12b88dbde26cb7c21e8c445b477a
ISIC <yuji3wang@gmail.com>
2018-07-28 00:08:37 -0400

    fixed compatibility issues in runEverything

--- utils/runEverything.bat
+++ utils/runEverything.bat
@@ -19,7 +19,7 @@ cd D:\tensorflow-models-master-2018-5\research\deeplab
 
 sh ./ISIC_run.sh
 xcopy /s D:\tensorflow-models-master-2018-5\research\deeplab\datasets\pascal_voc_seg\exp\train_on_trainval_set\vis\segmentation_results C:\Users\isica\Desktop\predict
-C:
-cd C:\Users\isica\Desktop
+D:
+cd D:\tensorflow-models-master-2018-5\research\deeplab\utils
 python jaccard.py
 
